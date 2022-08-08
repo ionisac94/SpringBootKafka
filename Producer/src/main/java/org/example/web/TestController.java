4ebf481315bd3061ba19ae4f7b39e28214049c71
Ion.Isac <ion.isac94@gmail.com>
2022-08-08 15:31:41 +0300

    Created integration test for controller

--- Producer/src/main/java/org/example/web/TestController.java
+++ Producer/src/main/java/org/example/web/TestController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.model.StudentDTO;
 import org.example.service.KafkaSender;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,13 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
-	private final KafkaSender kafkaSender;
+    private final KafkaSender kafkaSender;
 
 
-	@PostMapping("/{message}")
-	public ResponseEntity<Object> getText(@RequestBody StudentDTO student) {
-		kafkaSender.sendDataToKafka(student);
-		System.out.println("Produced message is: " + student);
-		return new ResponseEntity<>("The data was send to Kafka", HttpStatus.OK);
-	}
+    @PostMapping(value = "/message", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> getText(@RequestBody StudentDTO student) {
+        kafkaSender.sendDataToKafka(student);
+        System.out.println("Produced message is: " + student);
+        return new ResponseEntity<>("The data was send to Kafka", HttpStatus.OK);
+    }
 }
